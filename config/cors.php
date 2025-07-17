42ee9006e5a8e88b4b5f9e33d1c67adde26a9c31
xeankun <119888527+xeankun@users.noreply.github.com>
2025-07-17 13:16:23 +0800

    Fixing cors issue

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://tobaccoplastics.ggtc.world'],
+
 
     'allowed_origins_patterns' => [],
 
